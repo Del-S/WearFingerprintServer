fbe6c2d5547162141296e5cb7fc5e7a68a8159fd
Del-S <david@sucharda.cz>
2018-01-31 19:05:44 +0100

    Initiate couchebase with data from json file.

--- src/main/java/cz/uhk/beacon/fingerprint/api/controller/IndexController.java
+++ src/main/java/cz/uhk/beacon/fingerprint/api/controller/IndexController.java
@@ -1,5 +1,19 @@
 package cz.uhk.beacon.fingerprint.api.controller;
 
+import com.couchbase.client.deps.com.fasterxml.jackson.databind.ObjectMapper;
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import cz.uhk.beacon.fingerprint.api.model.Fingerprint;
+import cz.uhk.beacon.fingerprint.api.model.LocationEntry;
+import java.io.File;
+import java.io.FileReader;
+import java.util.UUID;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,6 +32,91 @@ public class IndexController {
     
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity getIndex(ModelMap model) {
+        return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
+    }
+    
+    @RequestMapping(path = "/couch_auto", method = RequestMethod.GET)
+    public ResponseEntity createCouchAuto(ModelMap model) {
+        Cluster cluster = CouchbaseCluster.create();
+        cluster.authenticate("Del_S", "deldas38");
+        Bucket bucket = cluster.openBucket("beacon");
+        
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONArray a = (JSONArray) jsonParser.parse(new FileReader("/home/del_s/Plocha/beacon.json"));
+            for (Object o : a) {
+
+                JSONObject beacon = (JSONObject) o;
+                JsonObject beaconJson = JsonObject.fromJson(beacon.toJSONString());
+                
+                bucket.upsert(JsonDocument.create((String) beacon.get("id"), beaconJson));
+                // parse your objects by means of parser.getXxxValue() and/or other parser's methods
+
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+        return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/couch", method = RequestMethod.GET)
+    public ResponseEntity createCouch(ModelMap model) {
+        // Initialize the Connection
+        Cluster cluster = CouchbaseCluster.create();
+        cluster.authenticate("Del_S", "deldas38");
+        Bucket bucket = cluster.openBucket("beacon");
+        
+        LocationEntry le = new LocationEntry();
+        le.setBuilding("UHK");
+        le.setFloor(2);
+        
+        Fingerprint f = new Fingerprint();
+        f.setX(20);
+        f.setY(25);
+        f.setScanStart(System.currentTimeMillis());
+        f.setScanEnd(System.currentTimeMillis()+20000);
+        f.setLevel("J2NP");
+        f.setLocationEntry(le);
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        String json = null;
+        try {
+             json = mapper.writeValueAsString(f);
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        
+        System.out.println("Testing");
+        System.out.println(json);
+        
+        /*if(json != null) {
+            JsonObject jsonObj = JsonObject.fromJson(json);
+            bucket.upsert(JsonDocument.create(f.getId().toString(), jsonObj));
+        }
+        
+        // Load the Document and print it
+        // Prints Content and Metadata of the stored Document
+        System.out.println(bucket.get(f.getId().toString()));
+        
+        // Create a N1QL Primary Index (but ignore if it exists)
+        /*bucket.bucketManager().createN1qlPrimaryIndex(true, false);
+
+        // Perform a N1QL Query
+        N1qlQueryResult result = bucket.query(
+            N1qlQuery.parameterized("SELECT name FROM bucketname WHERE $1 IN interests",
+            JsonArray.from("African Swallows"))
+        );
+
+        // Print each found Row
+        for (N1qlQueryRow row : result) {
+            // Prints {"name":"Arthur"}
+            System.out.println(row);
+        }*/
+        
+        cluster.disconnect();
+        
         return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
     }
     
---
+++ src/main/java/cz/uhk/beacon/fingerprint/api/model/Fingerprint.java
@@ -0,0 +1,225 @@
+package cz.uhk.beacon.fingerprint.api.model;
+
+import com.couchbase.client.deps.com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Fingerprint {
+
+    // Variables of this class
+    @JsonProperty("_id")
+    private UUID id;                                // UUID of this scan
+    private UUID scanID;                            // UUID to enable fingerprint grouping
+    private int x,y;                                // Calculated X and Y locations
+    private long scanLength;                        // Length of the scan in ms
+    @JsonProperty("timestamp")
+    private long scanStart;                         // Timestamps of scan start
+    @JsonProperty("finish")
+    private long  scanEnd;                          // Timestamps of scan end
+    /**
+     * @Deprecated
+     * Use LocationEntry instead
+     */
+    private String level;
+    private LocationEntry locationEntry;            // Location of fingerprint to enable multiple buildings and floors
+    private String deviceEntry;                // Device that created this fingerprint
+    private String beaconEntries;        // List of beacon entries scanned for this fingerprint
+    private String wirelessEntries;    // List of wireless entries scanned for this fingerprint
+    private String cellularEntries;    // List of cellular entries scanned for this fingerprint
+    private String sensorEntries;        // List of beacon entries scanned for this fingerprint
+
+    public Fingerprint() {
+        // Set id and scan UUID to send into other device
+        id = UUID.randomUUID();
+        scanID = UUID.randomUUID();
+
+        // Initiate lists
+        beaconEntries = "{entries:500}";
+        wirelessEntries = "{entries:300}";
+        cellularEntries = "{entries:200}";
+        sensorEntries = "{entries:300}";
+
+        // Set device
+        deviceEntry = "{\"entries\":\"1\"}";
+
+        // Default scan length is 60s
+        scanLength = 60000;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getScanID() {
+        return scanID;
+    }
+
+    public void setScanID(UUID scanID) {
+        this.scanID = scanID;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public long getScanLength() {
+        return scanLength;
+    }
+
+    public void setScanLength(long scanLength) {
+        this.scanLength = scanLength;
+    }
+
+    public long getScanStart() {
+        return scanStart;
+    }
+
+    public void setScanStart(long scanStart) {
+        this.scanStart = scanStart;
+    }
+
+    public long getScanEnd() {
+        return scanEnd;
+    }
+
+    public void setScanEnd(long scanEnd) {
+        this.scanEnd = scanEnd;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        if(level != null) {
+            this.locationEntry = new LocationEntry(level);
+        }
+        this.level = level;
+    }
+
+    public LocationEntry getLocationEntry() {
+        return locationEntry;
+    }
+
+    public void setLocationEntry(LocationEntry locationEntry) {
+        this.locationEntry = locationEntry;
+    }
+
+    public String getDeviceEntry() {
+        return deviceEntry;
+    }
+
+    public void setDeviceEntry(String deviceEntry) {
+        this.deviceEntry = deviceEntry;
+    }
+
+    public String getBeaconEntries() {
+        return beaconEntries;
+    }
+
+    public void setBeaconEntries(String beaconEntries) {
+        this.beaconEntries = beaconEntries;
+    }
+
+    public String getWirelessEntries() {
+        return wirelessEntries;
+    }
+
+    public void setWirelessEntries(String wirelessEntries) {
+        this.wirelessEntries = wirelessEntries;
+    }
+
+    public String getCellularEntries() {
+        return cellularEntries;
+    }
+
+    public void setCellularEntries(String cellularEntries) {
+        this.cellularEntries = cellularEntries;
+    }
+
+    public String getSensorEntries() {
+        return sensorEntries;
+    }
+
+    public void setSensorEntries(String sensorEntries) {
+        this.sensorEntries = sensorEntries;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Fingerprint fingerprint = (Fingerprint) o;
+        return Objects.equals(this.id, fingerprint.id) &&
+                Objects.equals(this.scanID, fingerprint.scanID) &&
+                Objects.equals(this.x, fingerprint.x) &&
+                Objects.equals(this.y, fingerprint.y) &&
+                Objects.equals(this.scanLength, fingerprint.scanLength) &&
+                Objects.equals(this.scanStart, fingerprint.scanStart) &&
+                Objects.equals(this.scanEnd, fingerprint.scanEnd) &&
+                Objects.equals(this.locationEntry, fingerprint.locationEntry) &&
+                Objects.equals(this.deviceEntry, fingerprint.deviceEntry) &&
+                Objects.equals(this.beaconEntries, fingerprint.beaconEntries) &&
+                Objects.equals(this.wirelessEntries, fingerprint.wirelessEntries) &&
+                Objects.equals(this.cellularEntries, fingerprint.cellularEntries) &&
+                Objects.equals(this.sensorEntries, fingerprint.sensorEntries);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, scanID, x, y, scanStart, scanEnd, locationEntry, deviceEntry, beaconEntries, wirelessEntries, cellularEntries, sensorEntries);
+    }
+
+
+    @Override
+    public String toString() {
+        return "class BeaconEntry {\n" +
+                "    id: " + toIndentedString(id) + "\n" +
+                "    scanID: " + toIndentedString(scanID) + "\n" +
+                "    x: " + toIndentedString(x) + "\n" +
+                "    y: " + toIndentedString(y) + "\n" +
+                "    scanLength: " + toIndentedString(scanLength) + "\n" +
+                "    scanStart: " + toIndentedString(scanStart) + "\n" +
+                "    scanEnd: " + toIndentedString(scanEnd) + "\n" +
+                "    locationEntry: " + toIndentedString(locationEntry) + "\n" +
+                "    deviceEntry: " + toIndentedString(deviceEntry) + "\n" +
+                "    beaconEntriesCount: " + toIndentedString(beaconEntries) + "\n" +
+                "    wirelessEntriesCount: " + toIndentedString(wirelessEntries) + "\n" +
+                "    cellularEntriesCount: " + toIndentedString(cellularEntries) + "\n" +
+                "    sensorEntriesCount: " + toIndentedString(sensorEntries) + "\n" +
+                "}";
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+}
