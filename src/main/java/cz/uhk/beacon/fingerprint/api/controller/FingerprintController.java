14a9f240605baff5371f8066216dad39b416881b
Del-S <david@sucharda.cz>
2018-02-01 03:08:13 +0100

    Basic of fingerprint contoller.

---
+++ src/main/java/cz/uhk/beacon/fingerprint/api/controller/FingerprintController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.uhk.beacon.fingerprint.api.controller;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.query.N1qlQueryResult;
+import com.couchbase.client.java.query.N1qlQueryRow;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/fingerprints")
+public class FingerprintController {
+    
+    private final static String UNAUTHORIZED = "Unauthorized";
+    
+    @RequestMapping(method = RequestMethod.GET, produces="application/json")
+    public ResponseEntity getFingerprints(HttpServletRequest request) {
+        
+        // UNAUTHORIZED exception when there is no app-id in the header
+        String deviceId = request.getHeader("deviceId");
+        if(StringUtils.isEmpty(deviceId)) {
+            //return new ResponseEntity<>(UNAUTHORIZED, null, HttpStatus.UNAUTHORIZED);
+        }
+        
+        Cluster cluster = CouchbaseCluster.create();
+        cluster.authenticate("admin", "admin123");
+        Bucket bucket = cluster.openBucket("beacon");
+        
+        bucket.bucketManager().createN1qlPrimaryIndex(true, false);
+        
+        List<JSONObject> result = new ArrayList();
+        JSONParser parser = new JSONParser();
+        
+        String timestamp = request.getParameter("timestamp");
+        if(timestamp == null) {
+            N1qlQueryResult queryResult = bucket.query(N1qlQuery.simple("SELECT * FROM `beacon`"));
+            
+            try {
+            
+                for (N1qlQueryRow row : queryResult.allRows()) {
+                    //we go straight for the raw bytes and turn into string
+                    String rowData = new String(row.byteValue());
+                    result.add((JSONObject)parser.parse(rowData));
+                }
+            
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new ResponseEntity<>(result, null, HttpStatus.OK);
+    }
+    
+}
--- src/main/java/cz/uhk/beacon/fingerprint/api/controller/IndexController.java
+++ src/main/java/cz/uhk/beacon/fingerprint/api/controller/IndexController.java
@@ -17,7 +17,6 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,12 +30,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity getIndex(ModelMap model) {
+    public ResponseEntity getIndex() {
         return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
     }
     
-    @RequestMapping(path = "/couch_auto", method = RequestMethod.GET)
-    public ResponseEntity createCouchAuto(ModelMap model) {
+    /*@RequestMapping(path = "/couch_auto", method = RequestMethod.GET)
+    public ResponseEntity createCouchAuto() {
         Cluster cluster = CouchbaseCluster.create();
         cluster.authenticate("Del_S", "deldas38");
         Bucket bucket = cluster.openBucket("beacon");
@@ -58,66 +57,5 @@ public class IndexController {
         }
         
         return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
-    }
-
-    @RequestMapping(path = "/couch", method = RequestMethod.GET)
-    public ResponseEntity createCouch(ModelMap model) {
-        // Initialize the Connection
-        Cluster cluster = CouchbaseCluster.create();
-        cluster.authenticate("Del_S", "deldas38");
-        Bucket bucket = cluster.openBucket("beacon");
-        
-        LocationEntry le = new LocationEntry();
-        le.setBuilding("UHK");
-        le.setFloor(2);
-        
-        Fingerprint f = new Fingerprint();
-        f.setX(20);
-        f.setY(25);
-        f.setScanStart(System.currentTimeMillis());
-        f.setScanEnd(System.currentTimeMillis()+20000);
-        f.setLevel("J2NP");
-        f.setLocationEntry(le);
-        
-        ObjectMapper mapper = new ObjectMapper();
-        
-        String json = null;
-        try {
-             json = mapper.writeValueAsString(f);
-        } catch (Exception e) {
-                e.printStackTrace();
-        }
-        
-        System.out.println("Testing");
-        System.out.println(json);
-        
-        /*if(json != null) {
-            JsonObject jsonObj = JsonObject.fromJson(json);
-            bucket.upsert(JsonDocument.create(f.getId().toString(), jsonObj));
-        }
-        
-        // Load the Document and print it
-        // Prints Content and Metadata of the stored Document
-        System.out.println(bucket.get(f.getId().toString()));
-        
-        // Create a N1QL Primary Index (but ignore if it exists)
-        /*bucket.bucketManager().createN1qlPrimaryIndex(true, false);
-
-        // Perform a N1QL Query
-        N1qlQueryResult result = bucket.query(
-            N1qlQuery.parameterized("SELECT name FROM bucketname WHERE $1 IN interests",
-            JsonArray.from("African Swallows"))
-        );
-
-        // Print each found Row
-        for (N1qlQueryRow row : result) {
-            // Prints {"name":"Arthur"}
-            System.out.println(row);
-        }*/
-        
-        cluster.disconnect();
-        
-        return new ResponseEntity<>("Fingerprint to Android server.", null, HttpStatus.OK);
-    }
-    
+    }*/
 }
